66a341d6536b6ed68e0e89545dce83751333c63a
Cartmaneze <komarov.n.a@yandex.ru>
2017-11-19 17:54:08 +0200

    19/11/17

---
+++ src/main/java/com/komarov/travelFood/model/anonymous/AnonimMealWithWeight.java
@@ -0,0 +1,25 @@
+package com.komarov.travelFood.model.anonymous;
+
+import com.komarov.travelFood.model.Meal;
+
+/**
+ * Created by Никита on 06.09.2017.
+ */
+public class AnonimMealWithWeight {
+    Meal meal;
+    int weight;
+
+    public AnonimMealWithWeight(Meal meal, int weight) {
+        this.meal = meal;
+        this.weight = weight;
+    }
+
+    public Meal getMeal() {
+        return meal;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
